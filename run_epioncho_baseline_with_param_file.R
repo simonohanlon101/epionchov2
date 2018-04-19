43fed6fccdd3fbc46087d55451c508bed7dd22f4
Simon O'Hanlon <simon.ohanlon@gmail.com>
2018-04-19 12:22:52 +0100

    paramters passed to R in wrong order

--- run_epioncho_baseline_with_param_file.R
+++ run_epioncho_baseline_with_param_file.R
@@ -19,7 +19,7 @@ pkg_load_or_install <- function( ... ){
 pkg_load_or_install("Rcpp","data.table")
 
 # change to WD of script
-setwd( args[2] )
+setwd( args[1] )
 #  does an output directory exist in the current WD?
 if (! dir.exists("output") ) {
   dir.create("output")
@@ -109,8 +109,8 @@ theta <- structure(list(ABR = 5000,
 
 
 #  Parameter values to alter in this simulation
-theta$ABR <- args[1]
-theta$kW0 <- args[2]
+theta$ABR <- args[2]
+theta$kW0 <- args[3]
 
 
 #  Output data.table
@@ -136,7 +136,7 @@ dt <- rbind( dt , as.list( out ) )
 
 #  Output file
 write.table( dt ,
-  file = paste0( "output/epioncho_renata_params_baseline___ABR_" , args[1] , "___k_" , args[2] , "___job_number_" , args[3] , ".txt" ) ,
+  file = paste0( "output/epioncho_renata_params_baseline___ABR_" , args[2] , "___k_" , args[3] , "___job_number_" , args[4] , ".txt" ) ,
   quote = FALSE ,
   sep = "\t" ,
   row.names = FALSE
